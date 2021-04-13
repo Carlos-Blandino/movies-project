f2f301674356e9686557179394b02bada6bb53b2
Carlos Blandino <cablandino66@gmail.com>
2021-04-12 17:03:39 -0700

    migration: getting error with predefined seed, making adjustments to movies description data type and seeding

--- Project_WeLoveMovies_1/src/app.js
+++ Project_WeLoveMovies_1/src/app.js
@@ -1,5 +1,16 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+// require your routers
+
+
+// require your error handlers
+
+
+// app.use
+app.use(express.json());
+app.use(cors);
+
 
 module.exports = app;
